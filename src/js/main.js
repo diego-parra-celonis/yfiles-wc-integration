9d9042765ca4d1b57a5011e37e212192cb3fb132
Diego Parra <d.parra@celonis.com>
2025-07-18 15:25:01 +0200

    Included method calls in web component

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const processDiagram = document.querySelector('#celonis-process-diagram');
     const processDiagramTitle = document.querySelector('#celonis-process-diagram-title');
     const processDiagramCounter = document.querySelector('#celonis-process-diagram-counter');
+    const processDiagramResetCounter = document.querySelector('#celonis-process-diagram-reset-counter');
 
     processDiagramTitle.addEventListener('input', (event) => {
         // Update the title of the web component when the input changes
-        if (processDiagram) {
-            processDiagram.title = event.target.value;
-        }
+        processDiagram.title = event.target.value;
+    });
+
+    processDiagramResetCounter.addEventListener('click', () => {
+        // Invokes a method within the web component 
+        processDiagram.resetCounter();
     });
     
     if (processDiagram) {
+        // Set initial value using directly the property of the web component
         processDiagram.title = processDiagramTitle.value || '';
 
         // Reads the event emmited by the web component and performs a visual update
